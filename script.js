e57e8e1bb767f9316c1800ce59d9cfd47648d90f
Kevin-Kolesky <kevinkolesky03@gmail.com>
2024-07-29 20:42:55 +0200

    Project completed

--- script.js
+++ script.js
@@ -2,15 +2,46 @@ const btnConvert = document.getElementById('convert-btn');
 const elInput = document.getElementById('number');
 const elOutput = document.getElementById('output');
 
-btnConvert.addEventListener( 'click', () => {
-    let number = elInput.value;
+const numerals = [
+    ["M",1000],
+    ["CM",900],
+    ["D",500],
+    ["CD",400],
+    ["C",100],
+    ["XC",90],
+    ["L",50],
+    ["XL",40],
+    ["X",10],
+    ["IX",9],
+    ["V",5],
+    ["IV",4],
+    ["I",1],
+]
 
-    if (number == '') {
-        elInput.textContent = "Please enter a valid number";
-    }else if (number == '-1') {
-        elInput.textContent = "Please enter a valid number";
-    } else if  (number >= 4000) {
-        elInput.textContent = "Please enter a number less or equal to 3999";
+btnConvert.addEventListener( 'keydown', e => {
+    if (e.key = "Enter"){
+        btnConvert.click()
     }
+})
 
+btnConvert.addEventListener('click', () => {
+    let value = elInput.value;
+
+    if (!value) {
+        elOutput.textContent = "Please enter a valid number";
+    }else if (value < 0) {
+        elOutput.textContent = "Please enter a number greater than or equal to 1";
+    } else if  (value >= 4000) {
+        elOutput.textContent = "Please enter a number less than or equal to 3999";
+    }
+    else {
+        let result = '';
+        for (const [roman, number] of numerals){
+            while (number <= value) {
+                result += roman;
+                value -= number;
+            }
+        }
+        elOutput.innerText = result;
+    }
 })
